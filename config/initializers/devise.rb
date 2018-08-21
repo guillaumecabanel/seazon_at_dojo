8135e3bfae1e1345b806cecef3db0c5ee2395515
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-08-21 18:34:53 +0200

    update view to reset passowrd

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '6ef1d401fb71f1d1466d83d56e10dcf849b7548d77e5c47dda66f011cb99d52aa14ea004c049d83a9e464cde3f6f886361413a5432b8cd5b5c752ff17c6c968a'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = ENV['DEVISE_EMAIL_SENDER']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
