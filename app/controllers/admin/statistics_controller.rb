66e7eea563b7c7ab5f61c909d3496ede35f2f1ce
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-08-24 18:34:01 +0200

    display the total number of orders for the given period in statistics

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -1,14 +1,15 @@
 module Admin
   class StatisticsController < Admin::BaseController
     def show
-      begin_date = params[:begin_date] ? Date.parse(params[:begin_date]) : Date.today - 14.days
-      end_date   = params[:end_date]   ? Date.parse(params[:end_date])   : Date.today
+      @begin_date = params[:begin_date] ? Date.parse(params[:begin_date]) : Date.today - 14.days
+      @end_date   = params[:end_date]   ? Date.parse(params[:end_date])   : Date.today
+      @orders_count = Order.where(created_at: @begin_date.beginning_of_day..@end_date.end_of_day).count
 
-      last_days_array = (begin_date..end_date).map do |day|
+      last_days_array = (@begin_date..@end_date).map do |day|
         helpers.l(day, format: "%a %d")
       end
 
-      orders_count_by_day_array = (begin_date..end_date).map do |day|
+      orders_count_by_day_array = (@begin_date..@end_date).map do |day|
         Order.where(created_at: day.beginning_of_day..day.end_of_day).count
       end
 
