327d68bcb4918cde969f2a756edae85fdc4731cf
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-08-24 17:54:11 +0200

    offer admin to choose begin_date and end_date through params for statistics

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -1,19 +1,20 @@
 module Admin
   class StatisticsController < Admin::BaseController
     def show
-      last_days_array = ((Date.today - 14.days)..Date.today).map do |day|
+      begin_date = params[:begin_date] ? Date.parse(params[:begin_date]) : Date.today - 14.days
+      end_date   = params[:end_date]   ? Date.parse(params[:end_date])   : Date.today
+
+      last_days_array = (begin_date..end_date).map do |day|
         helpers.l(day, format: "%a %d")
       end
 
-      orders_count_by_day_array = ((Date.today - 14.days)..Date.today).map do |day|
+      orders_count_by_day_array = (begin_date..end_date).map do |day|
         Order.where(created_at: day.beginning_of_day..day.end_of_day).count
       end
 
       @orders_data = {
         labels: last_days_array,
-        datasets: [
-            { values:orders_count_by_day_array }
-        ]
+        datasets: [{ values:orders_count_by_day_array }]
       }.to_json
     end
   end
