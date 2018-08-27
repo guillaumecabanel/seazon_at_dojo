fbfc9c5c6e49d0ddd82e90b403d9a972f3c51c13
Nicolas Filzi <nfilzi.webservices@gmail.com>
2018-08-27 23:34:51 +0200

    Replace N + 1 query on orders count by day with one fat query + map & reduce
    - Better naming for variables, don't need  suffixes
    - Use expected name key for datasets

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -17,27 +17,30 @@ module Admin
           values: [paid_orders, unpaid_orders]
         }]
       }.to_json
-
     end
-
-
 
     private
 
     def set_orders_by_day_data
-      last_days_array = (@begin_date..@end_date).map do |day|
+      last_days = (@begin_date..@end_date).map do |day|
         helpers.l(day, format: "%a %d")
       end
 
-      orders_count_by_day_array = (@begin_date..@end_date).map do |day|
-        Order.where(created_at: day.beginning_of_day..day.end_of_day).count
+      orders_count_by_day_with_missing_days = Order.
+        select('date(created_at) AS date, count(*) AS orders_count').
+        where(created_at: @begin_date.beginning_of_day..@end_date.end_of_day).
+        group("date(created_at)").
+        reduce({}) { |acc, order| acc[order.date] = order.orders_count; acc }
+
+      orders_count_by_day = (@begin_date..@end_date).map do |day|
+        orders_count_by_day_with_missing_days[day] || 0
       end
 
       @orders_by_day_data = {
-        labels: last_days_array,
+        labels: last_days,
         datasets: [{
-          names: "Commandes",
-          values:orders_count_by_day_array
+          name: "Commandes",
+          values: orders_count_by_day
         }]
       }.to_json
     end
