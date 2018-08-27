2fed79822432f1a3bac2aae9d06927f5ab20273b
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-08-27 10:38:50 +0200

    statistics wip

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -1,9 +1,10 @@
 module Admin
   class StatisticsController < Admin::BaseController
     def show
-      @begin_date = params[:begin_date] ? Date.parse(params[:begin_date]) : Date.today - 14.days
-      @end_date   = params[:end_date]   ? Date.parse(params[:end_date])   : Date.today
-      @orders_count = Order.where(created_at: @begin_date.beginning_of_day..@end_date.end_of_day).count
+      @begin_date   = params[:begin_date] ? Date.parse(params[:begin_date]) : Date.today - 14.days
+      @end_date     = params[:end_date]   ? Date.parse(params[:end_date])   : Date.today
+      @orders       = Order.where(created_at: @begin_date.beginning_of_day..@end_date.end_of_day)
+      @orders_count = @orders.count
 
       last_days_array = (@begin_date..@end_date).map do |day|
         helpers.l(day, format: "%a %d")
@@ -13,9 +14,12 @@ module Admin
         Order.where(created_at: day.beginning_of_day..day.end_of_day).count
       end
 
-      @orders_data = {
+      @orders_by_day_data = {
         labels: last_days_array,
-        datasets: [{ values:orders_count_by_day_array }]
+        datasets: [{
+          names: "Commandes",
+          values:orders_count_by_day_array
+        }]
       }.to_json
     end
   end
--- app/javascript/components/frappe_charts.js
+++ app/javascript/components/frappe_charts.js
@@ -2,20 +2,43 @@
 import { Chart } from 'frappe-charts/dist/frappe-charts.esm.js';
 import 'frappe-charts/dist/frappe-charts.min.css';
 
-function ordersChart() {
-  const ordersChartDiv = document.getElementById('orders-chart');
+function ordersByDayChart(ordersByDayChartDiv) {
+  const data = JSON.parse(ordersByDayChartDiv.dataset.json)
 
-  const data = JSON.parse(ordersChartDiv.dataset.orders)
-
-  const chart = new Chart(ordersChartDiv, {
+  const chart = new Chart(ordersByDayChartDiv, {
     data: data,
     type: 'bar',
     height: 140,
+    colors: ['#3273DC']
+  });
+}
+
+function ordersPaymentsChart(ordersPaymentsChartDiv) {
+  const data = {
+    labels: ["commandes payées", "commandes non payées"],
+    datasets: [{
+      values: [140, 30]
+    }]
+  }
+
+  const chart = new Chart(ordersPaymentsChartDiv, {
+    data: data,
+    type: 'percentage',
+    height: 140,
+    colors: ['#3273DC', '#FFDD57']
   });
 }
 
 function loadCharts() {
-  ordersChart();
+  const ordersByDayChartDiv = document.getElementById('orders-by-day-chart');
+  const ordersPaymentsChartDiv = document.getElementById('orders-payments-chart');
+
+  if (ordersByDayChartDiv) {
+    ordersByDayChart(ordersByDayChartDiv);
+  }
+  if (ordersPaymentsChartDiv) {
+    ordersPaymentsChart(ordersPaymentsChartDiv);
+  }
 }
 
 const frappeChartsDiv = document.getElementById('frappe-charts');
