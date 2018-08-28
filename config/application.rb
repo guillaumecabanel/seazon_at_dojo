0a6ad92adba5afdd16a239dfca21d73e3169e92e
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-08-28 22:15:44 +0200

    config timezone to 'Paris'

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module SeazonAtDojo
   class Application < Rails::Application
+    config.time_zone = 'Paris'
+
     config.generators do |generate|
       generate.assets false
       generate.helper false
