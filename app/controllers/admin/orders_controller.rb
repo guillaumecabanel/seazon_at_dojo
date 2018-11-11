8f41ca3b84e93947098ef80b47017f55e4e8f243
Nicolas Filzi <nfilzi.webservices@gmail.com>
2018-11-11 15:17:37 +0100

    Update app/controllers/admin/orders_controller.rb
    Co-Authored-By: guillaumecabanel <guillaumecabanel@users.noreply.github.com>

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,7 +8,7 @@ module Admin
         Order.order(created_at: :desc).page(params[:page]).per(15)
       end
 
-      @orders_by_week = @orders.group_by{ |order| order.created_at.beginning_of_week }
+      @orders_by_week = @orders.group_by { |order| order.created_at.beginning_of_week }
     end
   end
 end
